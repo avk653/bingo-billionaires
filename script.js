955fbd6c99765862debea7a15495b41e7baf4f43
McNeil, Evelyn <elm5001@hw.ac.uk>
2025-10-23 10:01:57 +0000

    javascript for accounts page

---
+++ script.js
@@ -0,0 +1,11 @@
+function logIn(){
+    document.getElementById("base").style.display = "none";
+    document.getElementById("signIn").style.display = "block";
+    document.getElementById("createAccount").style.display = "none";
+}
+
+function createAccount(){
+    document.getElementById("base").style.display = "none";
+    document.getElementById("signIn").style.display = "none";
+    document.getElementById("createAccount").style.display = "block";
+}
